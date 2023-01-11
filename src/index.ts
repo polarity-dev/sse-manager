129cafa95a53ca10a4fd4143a895e9e6fa67985d
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2023-01-11 16:57:17 +0100

    fix in types path in package.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,29 @@ import type { createClient } from "redis"
 import type { Response } from "express"
 import { EventEmitter } from "events"
 import { randomUUID } from "crypto"
-import { EventsAdapterEmitFn, EventsAdapterInitFn, EventsAdapterOnFn, HTTPAdapterSetResHeadersFn, HTTPAdapterWriteResFn, HTTPEndResHeadersFn, HTTPFlushResHeadersFn, HTTPResOnCloseCallbackFn, SSEManagerOptions, SSEMessage, SSEStreamOptions } from "./types"
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type SSEManagerOptions = {
+  httpAdapter?: HTTPAdapter,
+  eventsAdapter?: EventsAdapter,
+  keepAliveInterval?: number | null
+}
+
+export type SSEMessage = { data: string, id?: number | string,  channel?: string, retry?: number }
+
+export type SSEStreamOptions = { keepAliveInterval: number | null }
+
+export type HTTPAdapterSetResHeadersFn = (res: any, headers: { [key: string]: string }) => void
+export type HTTPAdapterWriteResFn = (res: any, data: string) => void
+export type HTTPFlushResHeadersFn = (res: any) => void
+export type HTTPEndResHeadersFn = (res: any) => void
+export type HTTPResOnCloseCallbackFn = (res: any, fn: () => void) => void
+
+
+export type EventsAdapterEmitFn = (event: string, data: string) => Promise<void>
+export type EventsAdapterOnFn = (event: string, fn: (data: string, event: string) => void) => Promise<void>
+export type EventsAdapterInitFn = () => Promise<void>
+
 
 export class SSEManager extends EventEmitter {
   readonly id: string
